e38c506877252245a167a7c3826db32f192e7a88
SkyeGamesYT <113136719+SkyeGamesYT@users.noreply.github.com>
2024-04-10 09:42:37 -0500

    Create FreezeServer.lua

---
+++ CMDR Freeze/FreezeServer.lua
@@ -0,0 +1,9 @@
+--Made by _skyelol_ on Discord
+--Must be a ModuleScript
+
+
+return function (context, player: Player)
+	if player and player.Character then
+		player.Character.HumanoidRootPart.Anchored = true
+	end
+end
